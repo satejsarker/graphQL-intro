098f83247dcc72ecce854cdade708ec5ecc4acc2
satejsarker <>
2018-10-04 17:51:52 +0600

    mutile data process activated with grapql

--- schema/schema.js
+++ schema/schema.js
@@ -2,9 +2,12 @@ const graphql =require ('graphql');
 const _ =require ('lodash');
 //dummy data 
 var books=[
-    {name:'Now i am with you',genre:'Romance',id:'1'},
-    {name:'Now you see me ',genre:'Fantasy',id:'2'},
-    {name:'Basic',genre:'Criame',id:'3'},
+    {name:'Now i am with you',genre:'Romance',id:'1',authorId:'1'},
+    {name:'Now you see me ',genre:'Fantasy',id:'2',authorId:'2'},
+    {name:'Basic',genre:'Criame',id:'3',authorId:'3'},
+
+{    name:'now you see me ',genre:'Fantasy',id:'4',authorId:'2'},
+{    name:'hero of our socity ',genre:'social  ',id:'5',authorId:'2'}
 ]; 
 
 var authors=[
@@ -13,14 +16,23 @@ var authors=[
     {name:'samir',age:55,id:'3'},
 ];
 const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,
-    GraphQLInt
+    GraphQLInt,GraphQLList
 }=graphql;
 const BookType=new GraphQLObjectType({
     name:'book',
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
-        genre:{type:GraphQLString}
+        genre:{type:GraphQLString},
+        //nested data qurry 
+        author:{
+            type:AuthorType,
+            resolve(parent,args){
+                //cz only one author for perticular book 
+                    console.log(parent);
+                    return _.find(authors,{id:parent.authorId})
+            }        
+        }
     })
 });
 const AuthorType=new GraphQLObjectType({
@@ -28,7 +40,14 @@ const AuthorType=new GraphQLObjectType({
     fields:()=>({
         id:{type:GraphQLID},
         name:{type:GraphQLString},
-        age:{type:GraphQLInt}
+        age:{type:GraphQLInt},
+        books:{
+            type:new GraphQLList(BookType),
+            resolve(parent,args){
+                //one author can have multiple book 
+                    return _.filter(books,{authorId:parent.id})
+            }
+        }
     })
 });
 // graphQl schema desgin 
@@ -54,6 +73,19 @@ const RoootQury=new GraphQLObjectType({
             args:{id:{type:GraphQLID}},
             resolve(parent,args){
                 return _.find(authors,{id:args.id})
+            }
+        },
+        //for all books
+        books:{
+            type:new  GraphQLList(BookType),
+            resolve(parent,args){
+                return books;
+            }
+        },
+        authors:{
+            type:new GraphQLList(AuthorType),
+            resolve(parent,args){
+                return authors;
             }
         }
     }
